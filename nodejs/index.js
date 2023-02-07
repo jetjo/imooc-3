7302281407f164819b0fc7899fd8bab772938383
yangbing007 <yangbinggoooooogle@gmail.com>
2023-02-07 17:41:33 +0800

    学习node debug

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,1 +1,14 @@
 const http = require('http')
+
+const server = http.createServer((req, res) =>
+{
+    debugger;
+    a();
+    const url = req.url
+    const path = url.split('?')[0]
+    res.end(path);
+})
+
+server.listen(3000);
+
+console.log('服务已经在3000上启动！');
