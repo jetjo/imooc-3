4ead67b05d24971fde68a82f7d0b38d1ed184297
yangbing007 <yangbinggoooooogle@gmail.com>
2023-02-09 11:14:47 +0800

    请求参数

--- full-stack-base/learn-nodejs/learn-koa2-1/routes/message-board.js
+++ full-stack-base/learn-nodejs/learn-koa2-1/routes/message-board.js
@@ -4,12 +4,26 @@ router.prefix('/api/message-board');
 
 router.get('/', async (ctx, next) =>
 {
+  console.log('请求的查询参数：', ctx.query);
   ctx.body = {
     errno: 0,
     data: [
       { id: 0, 'user': '张三', content: '留言1' },
       { id: 1, 'user': '李四', content: '留言2' },
     ]
+  };
+})
+
+router.post('/', async (ctx, next) =>
+{
+  const body = ctx.request.body;
+  console.log('请求的body：', body);
+  ctx.body = {
+    errno: 0,
+    data: {
+      ...body,
+      id: parseInt(Math.random() * 10000)
+    }
   };
 })
 
