a2b1be646e078509c59dbe6f31d74f1ed462b1b3
yangbing007 <yangbinggoooooogle@gmail.com>
2023-04-17 21:18:24 +0800

    readonly

---
+++ learn-vue-3/src/响应原理/readonly/ref.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { ref, readonly } from "vue";
+const refBaseType = ref(0);
+const readonlyBaseTypeRef = readonly(refBaseType);
+setTimeout(() => {
+  console.log({ refBaseType, readonlyBaseTypeRef });
+  console.log(readonlyBaseTypeRef.value);
+  //   readonlyBaseTypeRef.value = Math.random();
+  refBaseType.value = Math.random();
+  console.log(readonlyBaseTypeRef.value);
+}, 5000);
+</script>
+
+<style lang="scss" scoped></style>
---
+++ learn-vue-3/src/响应原理/readonly/refObj.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { ref, readonly } from "vue";
+const refObj = ref({ age: 0 });
+const readonlyObjRef = readonly(refObj);
+setTimeout(() => {
+  console.log({ refObj, readonlyObjRef });
+  console.log(readonlyObjRef.value.age);
+  //   readonlyObjRef.value.age = Math.random();
+  refObj.value.age = Math.random();
+  console.log(readonlyObjRef.value.age);
+}, 5000);
+</script>
+
+<style lang="scss" scoped></style>
